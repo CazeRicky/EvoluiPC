0944df5733561294435dffc986e86b6662e9e5de
David Alenor <david.alenor@ufrpe.br>
2026-04-22 00:05:57 -0300

    Configurações simples

--- django/evoluipc_backend/urls.py
+++ django/evoluipc_backend/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import include, path
 
 # Rotas raiz do projeto Django.
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
